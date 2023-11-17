835aecdc188a40d46d8bfcd44b99a7c00eee6de9
SAJJAD <122774486+snoou@users.noreply.github.com>
2023-11-17 14:40:41 +0330

    Add files via upload

---
+++ یک سوال ساده/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int input = scanner.nextInt();
+
+        if (input <= 100 && input>=0){
+
+            for (int i = 0 ; i < input ; i++){
+
+                System.out.println("man khoshghlab hastam");
+
+            }
+
+        }
+
+    }
+
+}
